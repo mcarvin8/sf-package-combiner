78dc614168701171b0a3d0f3b5b39126c24763dd
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-05-27 11:48:19 -0400

    fix: remove dead code

--- src/core/writePackage.ts
+++ src/core/writePackage.ts
@@ -2,18 +2,11 @@ import { writeFile } from 'node:fs/promises';
 import { XMLBuilder } from 'fast-xml-parser';
 import { PackageManifestObject } from '@salesforce/source-deploy-retrieve';
 
-import { sfXmlns, xmlConf } from '../utils/constants.js';
+import { xmlConf } from '../utils/constants.js';
 
 export async function writePackage(packageXmlObject: PackageManifestObject, combinedPackage: string): Promise<void> {
   const builder = new XMLBuilder(xmlConf);
   let xmlContent = builder.build(packageXmlObject) as string;
-
-  if (Array.isArray(packageXmlObject.Package.types) && packageXmlObject.Package.types.length === 0) {
-    xmlContent = xmlContent.replace(
-      `<Package xmlns="${sfXmlns}"></Package>`,
-      `<Package xmlns="${sfXmlns}">\n\n</Package>`
-    );
-  }
 
   if (packageXmlObject.Package.version === '0.0') {
     xmlContent = xmlContent.replace(/^\s*<version>0\.0<\/version>\s*\r?\n?/gm, '');
